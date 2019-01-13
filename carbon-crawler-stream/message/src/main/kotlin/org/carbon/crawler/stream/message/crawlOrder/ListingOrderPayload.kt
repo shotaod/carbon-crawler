a50797868d5a4cd81d1bcc6647a0544d9590d85c
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 22:36:14 +0900

    add message interface module

--- carbon-crawler-stream/message/src/main/kotlin/org/carbon/crawler/stream/message/crawlOrder/ListingOrderPayload.kt
+++ carbon-crawler-stream/message/src/main/kotlin/org/carbon/crawler/stream/message/crawlOrder/ListingOrderPayload.kt
@@ -3,7 +3,6 @@ package org.carbon.crawler.stream.message.crawlOrder
 /**
  * @author Soda 2018/08/09.
  */
-data class CrawlOrderPayload(
-        val dictionaryId: Long,
-        val url: String
+data class ListingOrderPayload(
+    val hostId: Long
 )
