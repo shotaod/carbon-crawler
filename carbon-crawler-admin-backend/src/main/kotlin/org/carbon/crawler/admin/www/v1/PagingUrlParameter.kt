29d5087b5e9ac7e052f7c2916172a0e1277a94fe
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 23:01:30 +0900

    add Generic parameter data class

--- carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/www/v1/PagingUrlParameter.kt
+++ carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/www/v1/PagingUrlParameter.kt
@@ -1,4 +1,4 @@
-package org.carbon.crawler.admin.www.v1.dictionary
+package org.carbon.crawler.admin.www.v1
 
 import org.carbon.crawler.admin.extend.carbon.validation.NumberMax
 import org.carbon.crawler.admin.extend.carbon.validation.NumberMin
@@ -11,36 +11,35 @@ import org.carbon.objects.validation.matcher.be
 import org.carbon.objects.validation.matcher.mayBe
 
 
-class GetDictionaryParameter(
-        val _page: String,
-        val _size: String
-) : Validated<GetDictionaryParameter> by GetDictionaryParameterSchema {
+class PagingUrlParameter(
+    val _page: String,
+    val _size: String
+) : Validated<PagingUrlParameter> by PagingUrlParameterSchema {
     companion object {
-        fun get(page: String?, size: String?): GetDictionaryParameter =
-                GetDictionaryParameter(page ?: "0", size ?: "10")
+        fun get(page: String?, size: String?): PagingUrlParameter =
+            PagingUrlParameter(page ?: "0", size ?: "10")
     }
 
     operator fun component1(): Int = _page.toInt()
     operator fun component2(): Int = _size.toInt()
 }
 
-object GetDictionaryParameterSchema : Validated<GetDictionaryParameter> {
-    override val def: Definition<GetDictionaryParameter> = { param ->
+object PagingUrlParameterSchema : Validated<PagingUrlParameter> {
+    override val def: Definition<PagingUrlParameter> = { param ->
         param._page.should {
             and(
-                    it be NumberString,
-                    it.toIntOrNull() mayBe NumberMin(0)
+                it be NumberString,
+                it.toIntOrNull() mayBe NumberMin(0)
             )
         } otherwise "page".invalidate()
 
         param._size.should {
             val num = it.toIntOrNull()
             and(
-                    it mayBe NumberString,
-                    num mayBe NumberMin(0),
-                    num mayBe NumberMax(99)
+                it mayBe NumberString,
+                num mayBe NumberMin(0),
+                num mayBe NumberMax(99)
             )
         } otherwise "size".invalidate()
-
     }
 }
