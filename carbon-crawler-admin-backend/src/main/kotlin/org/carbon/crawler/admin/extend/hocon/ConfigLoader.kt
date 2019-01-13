440ed0d4614c773f8473514ef24667ddf84c0cf3
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 22:45:29 +0900

    deal with profile switching

---
+++ carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/extend/hocon/ConfigLoader.kt
@@ -0,0 +1,16 @@
+package org.carbon.crawler.admin.extend.hocon
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
+
+object ConfigLoader {
+    private val logger = LoggerFactory.getLogger(ConfigLoader::class.java)
+    private val config: Config by lazy {
+        val profile = System.getProperty("carbon.profile") ?: "dev"
+        logger.info("profile: {}", profile)
+        ConfigFactory.load().getConfig(profile)
+    }
+
+    fun <T> withConfig(cb: Config.() -> T) = config.run(cb)
+}
