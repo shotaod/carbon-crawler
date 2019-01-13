7f9d144a74a95d0b3599b7423a4e787b16c47c7c
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 22:45:11 +0900

    add validation matcher(isNumber, isXpath, isOneOf)

--- carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/extend/carbon/validation/StringMatcher.kt
+++ carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/extend/carbon/validation/StringMatcher.kt
@@ -12,11 +12,31 @@ import org.carbon.objects.validation.matcher.reject
 val LengthMin: (i: Int) -> BeCounterExpression<String> = { i -> { this min i } }
 val LengthMax: (i: Int) -> BeCounterExpression<String> = { i -> { this max i } }
 val NumberString: BeCounterExpression<String> = { this.isNumberString() }
+val XPath: BeCounterExpression<String> = { this.isXpath() }
+@Suppress("FunctionName")
+fun OneOf(vararg value: String): BeCounterExpression<String> = { this.oneOf(*value) }
 
 fun String.isNumberString(): Evaluation = this.toIntOrNull()
-        ?.let { Evaluation.Accepted }
-        ?: this.reject(
-                Code("String.NumberString"),
-                ParamList(emptyList<String>()),
-                "Not a number"
-        )
+    ?.let { Evaluation.Accepted }
+    ?: this.reject(
+        Code("String.NumberString"),
+        ParamList(emptyList<String>()),
+        "Not a number"
+    )
+
+val xPathReg = "xpath://.+".toRegex()
+fun String.isXpath(): Evaluation =
+    if (xPathReg.matches(this)) Evaluation.Accepted
+    else this.reject(
+        Code("String.XPath"),
+        ParamList(emptyList<String>()),
+        "Not a valid xpath"
+    )
+
+fun String.oneOf(vararg value: String): Evaluation =
+    if (value.any { this === it }) Evaluation.Accepted
+    else this.reject(
+        Code("String.OneOf"),
+        ParamList(value.toList()),
+        "Not match"
+    )
