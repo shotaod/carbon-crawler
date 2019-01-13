60cc6e92797e1a3de561417411d80b0ad3587654
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 22:31:04 +0900

    fix protocol

--- carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/extend/selenium/Misc.kt
+++ carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/extend/selenium/Misc.kt
@@ -6,7 +6,7 @@ import org.openqa.selenium.By
  * @author Soda 2018/08/10.
  */
 fun String.toBy(): By {
-    val xPathProtocol = "xpath:"
+    val xPathProtocol = "xpath://"
     if (this.startsWith(xPathProtocol)) return By.ByXPath(this.removePrefix(xPathProtocol))
 
     throw UnsupportedOperationException()
