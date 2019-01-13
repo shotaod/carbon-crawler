0e03a1eb84d70dc7907108d3f7014ff5f6909b21
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-13 22:45:47 +0900

    more generic

--- carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/extend/ktor/ReservedException.kt
+++ carbon-crawler-admin-backend/src/main/kotlin/org/carbon/crawler/admin/extend/ktor/ReservedException.kt
@@ -1,8 +1,8 @@
-package org.carbon.crawler.admin.aservice
+package org.carbon.crawler.admin.extend.ktor
 
 import io.ktor.http.HttpStatusCode
 
-abstract class UseCaseException(
+abstract class ReservedException(
         val status: HttpStatusCode,
         val clientMessage: String
 ) : Exception()
