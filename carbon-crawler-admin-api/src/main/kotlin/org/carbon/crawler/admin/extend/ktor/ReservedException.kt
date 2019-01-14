705c27f31aa861b5422e6eabfd15ab7e580b6642
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-01-15 00:21:46 +0900

    fix indent

--- carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/ktor/ReservedException.kt
+++ carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/ktor/ReservedException.kt
@@ -3,6 +3,6 @@ package org.carbon.crawler.admin.extend.ktor
 import io.ktor.http.HttpStatusCode
 
 abstract class ReservedException(
-        val status: HttpStatusCode,
-        val clientMessage: String
+    val status: HttpStatusCode,
+    val clientMessage: String
 ) : Exception()
