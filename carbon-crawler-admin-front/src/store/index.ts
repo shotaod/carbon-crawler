5f593bc41a17696f5fec28181b9a60037acd4c10
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-14 17:00:59 +0900

    switch env whether use devtool or not

--- carbon-crawler-admin-front/src/store/index.ts
+++ carbon-crawler-admin-front/src/store/index.ts
@@ -4,6 +4,7 @@ import createSagaMiddleware, {END} from 'redux-saga'
 import {rootReducer} from '../reducer'
 import {routerMiddleware as createRouterMiddleware} from "react-router-redux";
 import {History} from "history";
+import {env} from "../config";
 
 type Saga = {
   run: (any: any) => any
@@ -13,9 +14,12 @@ type Saga = {
 type reduxWindow = {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any
 }
-const enhancedCompose = (window as reduxWindow).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-  trace: true,
-}) || compose
+const enhancedCompose = (() => {
+  if (env.isProd) return compose;
+  const devToolCompose = (window as reduxWindow).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  if (devToolCompose) return devToolCompose({trace: true})
+  else return compose
+})()
 
 type SagaStore<S = any, A extends Action = AnyAction> = Store<S, A> & Saga
 
