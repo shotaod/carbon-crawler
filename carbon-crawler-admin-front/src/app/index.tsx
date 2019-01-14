f8a54a0319b9f51bced7b8de284f8a0a998f9f92
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-14 16:55:59 +0900

    switch hot reload config

--- carbon-crawler-admin-front/src/app/index.tsx
+++ carbon-crawler-admin-front/src/app/index.tsx
@@ -2,8 +2,10 @@ import {compose} from 'recompose'
 import {History} from 'history'
 import {hot} from 'react-hot-loader'
 import {RoutesContainer} from '../route'
+import {env} from "../config";
 
+console.log(env.isProd, env.isProd)
 // if dev
-export const Application = compose<{ history: History }, { history: History }>(
-  hot(module),
-)(RoutesContainer)
+export const Application = env.isDev
+  ? compose<{ history: History }, { history: History }>(hot(module))(RoutesContainer)
+  : RoutesContainer
