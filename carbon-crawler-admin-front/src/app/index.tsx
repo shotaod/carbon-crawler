cf5590ef6ecf9ef735a4c51d9e91ee4613a4b8f9
ShotaOda <shota.oda@bizreach.co.jp>
2019-01-14 17:00:34 +0900

    remove console.log

--- carbon-crawler-admin-front/src/app/index.tsx
+++ carbon-crawler-admin-front/src/app/index.tsx
@@ -4,8 +4,6 @@ import {hot} from 'react-hot-loader'
 import {RoutesContainer} from '../route'
 import {env} from "../config";
 
-console.log(env.isProd, env.isProd)
-// if dev
 export const Application = env.isDev
   ? compose<{ history: History }, { history: History }>(hot(module))(RoutesContainer)
   : RoutesContainer
