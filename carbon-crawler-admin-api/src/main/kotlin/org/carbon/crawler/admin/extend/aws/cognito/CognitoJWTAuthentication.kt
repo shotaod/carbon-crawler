0b05b6958a7811deffc8ee90973addbefd7c2387
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-01-15 22:10:23 +0900

    encapsulate converter function

--- carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/aws/cognito/CognitoJWTAuthentication.kt
+++ carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/aws/cognito/CognitoJWTAuthentication.kt
@@ -1,27 +1,37 @@
 package org.carbon.crawler.admin.extend.aws.cognito
 
-import com.typesafe.config.Config
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.auth.Authentication
 import io.ktor.auth.jwt.JWTPrincipal
 import io.ktor.auth.jwt.jwt
 import org.carbon.crawler.admin.extend.ktor.auth.AuthenticationRegisterer
 
-object CognitoJWTAuthentication : AuthenticationRegisterer {
-    override fun enable(authentication: Authentication.Configuration, param: Config) {
+
+object CognitoJWTAuthentication : AuthenticationRegisterer<CognitoConfig> {
+    override fun enable(authentication: Authentication.Configuration, param: CognitoConfig) {
         with(authentication) {
             jwt {
                 realm = "carbon crawler admin api"
-                verifier(
-                    CognitoConfig(
-                        param.getString("region"),
-                        param.getString("poolId"),
-                        param.getString("clientId")
-                    ).toVerifier()
-                )
+                verifier(param.toVerifier())
                 validate {
                     JWTPrincipal(it.payload)
                 }
             }
         }
+    }
+
+    private fun CognitoConfig.toVerifier(): JWTVerifier {
+        val cognitoIdpUrl = "https://cognito-idp.$region.amazonaws.com/$userPoolId"
+        val publicKeyEndpoint = ".well-known/jwks.json"
+
+        val keyProvider = CachedPublicKeyProvider("$cognitoIdpUrl/$publicKeyEndpoint")
+
+        return JWT.require(Algorithm.RSA256(keyProvider))
+            .withAudience(clientId)
+            .withIssuer(cognitoIdpUrl)
+            .withClaim("token_use", "id")
+            .build()
     }
 }
