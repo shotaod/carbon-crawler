f30bec7d4d5ddc5ffe42cf56fca081da5160f0f0
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-01-15 22:09:44 +0900

    making just config dto

--- carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/aws/cognito/JWTVerifierFactory.kt
+++ carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/extend/aws/cognito/JWTVerifierFactory.kt
@@ -1,24 +1,7 @@
 package org.carbon.crawler.admin.extend.aws.cognito
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
-
 data class CognitoConfig(
     val region: String,
     val userPoolId: String,
     val clientId: String
 )
-
-fun CognitoConfig.toVerifier(): JWTVerifier {
-    val cognitoIdpUrl = "https://cognito-idp.${region}.amazonaws.com/${userPoolId}"
-    val publicKeyEndpoint = ".well-known/jwks.json"
-
-    val keyProvider = CachedPublicKeyProvider("$cognitoIdpUrl/$publicKeyEndpoint")
-
-    return JWT.require(Algorithm.RSA256(keyProvider))
-        .withAudience(clientId)
-        .withIssuer(cognitoIdpUrl)
-        .withClaim("token_use", "id")
-        .build()
-}
