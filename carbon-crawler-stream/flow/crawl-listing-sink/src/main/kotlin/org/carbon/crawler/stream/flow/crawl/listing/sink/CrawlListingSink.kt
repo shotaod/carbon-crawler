8ef8e6a99242fec32f3b264e58465894f6f92004
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-04 23:14:46 +0900

    update logging style

--- carbon-crawler-stream/flow/crawl-listing-sink/src/main/kotlin/org/carbon/crawler/stream/flow/crawl/listing/sink/CrawlListingSink.kt
+++ carbon-crawler-stream/flow/crawl-listing-sink/src/main/kotlin/org/carbon/crawler/stream/flow/crawl/listing/sink/CrawlListingSink.kt
@@ -1,89 +1,97 @@
 package org.carbon.crawler.stream.flow.crawl.listing.sink
 
-import org.carbon.crawler.model.domain.HostRepository
+import org.carbon.crawler.model.domain.QueryRepository
 import org.carbon.crawler.model.extend.exposed.batchInsertV2
 import org.carbon.crawler.model.infra.record.HostTable
 import org.carbon.crawler.model.infra.record.PageAttributeTable
 import org.carbon.crawler.model.infra.record.PageTable
 import org.carbon.crawler.stream.core.extend.selenium.DriverFactory
 import org.carbon.crawler.stream.core.extend.selenium.toBy
-import org.carbon.crawler.stream.core.extend.spring.cloud.TypedSink
-import org.carbon.crawler.stream.message.crawlOrder.ListingOrderPayload
+import org.carbon.crawler.stream.core.extend.spring.cloud.SinkFunction
+import org.carbon.crawler.stream.message.TargetHostPayload
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.sql.statements.BatchInsertStatement
+import org.openqa.selenium.remote.RemoteWebDriver
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import kotlin.properties.Delegates
 
 private val log: Logger = LoggerFactory.getLogger("crawl.listing.sink")
-fun crawlListingSink(driverFactory: DriverFactory): TypedSink<ListingOrderPayload> = { (hostId) ->
-    HostRepository.fetchBy(hostId)
-        ?.let { entity ->
-            driverFactory.use { driver ->
-                // ______________________________________________________
-                //
-                // @ in listing page
-                driver.get(entity.listingUrl)
-                val detailPathEls = driver.findElements(entity.listingQuery.toBy())
-                val data = detailPathEls
-                    .map {
-                        // ______________________________________________________
-                        //
-                        // @ in detail page
-                        val href = it.getAttribute("href")
-                        log.info("[{}], findATag: @href: {}", entity.listingUrl, href)
-                        href
-                    }.map { href ->
-                        driver.get(href)
-                        val title = driver.title
-                        val attributes = entity.detailQueries
-                            .map { detail ->
-                                val query = detail.query.toBy()
-                                val value = driver.findElement(query).text
-                                log.info("[{}], queryAttribute @xpath: {} @text: {}", entity.listingUrl, query, value)
-                                PageAttributeStatement(detail.queryName, value)
-                            }
-                        PageStatement(
-                            hostId,
-                            title,
-                            href,
-                            attributes
-                        )
-                    }
+private fun logInfo(driver: RemoteWebDriver, vararg info: Pair<String, String>) =
+    log.info(mapOf("driver" to driver.currentUrl, *info).toList().joinToString(",") { (key, value) -> "[$key]:$value" })
 
-                // ______________________________________________________
-                //
-                // @ batch inserts
-                // - pages
-                // - attributes
-                val now = LocalDateTime.now()
-                val pageIds = PageTable.batchInsertV2(data,
-                    onDuplicate = {
-                        this[PageTable.title] = it.title
-                        this[PageTable.updatedAt] = now
-                    },
-                    body = {
-                        it.applyInsert(this)
-                    })
-                // attributes
-                val attributeData = data.withIndex()
-                    .flatMap { (index, value) ->
-                        val pageId = pageIds[index]
-                        value.attributes.map { it.withPageId(pageId) }
+fun crawlListingSink(driverFactory: DriverFactory): SinkFunction<TargetHostPayload> = fun(payload: TargetHostPayload) {
+    val (hostId) = payload
+    val entity = QueryRepository.fetchBy(hostId)
+    if (entity === null) {
+        log.info("host not found for id {} ", hostId)
+        return
+    }
+
+    val data = driverFactory.use { driver ->
+        // ______________________________________________________
+        //
+        // @ in listing page
+        logInfo(driver, "action" to "trying to get ${entity.listingUrl}")
+        driver.get(entity.listingUrl)
+        logInfo(driver, "action" to "driver.get", "page_source" to driver.pageSource.replace("\n", ""))
+
+        val detailPathEls = driver.findElements(entity.listingQuery.toBy())
+        detailPathEls
+            .map { it.getAttribute("href") }
+            .onEach { logInfo(driver, "action" to "driver.findElements", "href" to it) }
+            // ______________________________________________________
+            //
+            // @ in detail page
+            .map { href ->
+                driver.get(href)
+                val title = driver.title
+                val attributes = entity.detailQueries
+                    .map { detail ->
+                        val query = detail.query.toBy()
+                        val value = driver.findElement(query).text
+                        logInfo(driver, "action" to "driver.findElement", "xpath" to detail.query, "value" to value)
+                        PageAttributeStatement(detail.queryName, value)
                     }
-                PageAttributeTable.batchInsertV2(attributeData,
-                    onDuplicate = {
-                        this[PageAttributeTable.value] = it.value
-                        this[PageAttributeTable.updatedAt] = now
-                    },
-                    body = {
-                        it.applyInsert(this)
-                    })
+                PageStatement(
+                    hostId,
+                    title,
+                    href,
+                    attributes
+                )
             }
-        }
-}
+    }
 
+    // ______________________________________________________
+    //
+    // @ batch inserts
+    // - pages
+    // - attributes
+    val now = LocalDateTime.now()
+    val pageIds = PageTable.batchInsertV2(data,
+        onDuplicate = {
+            this[PageTable.title] = it.title
+            this[PageTable.updatedAt] = now
+        },
+        body = {
+            it.applyInsert(this)
+        })
+    // attributes
+    val attributeData = data.withIndex()
+        .flatMap { (index, value) ->
+            val pageId = pageIds[index]
+            value.attributes.map { it.withPageId(pageId) }
+        }
+    PageAttributeTable.batchInsertV2(attributeData,
+        onDuplicate = {
+            this[PageAttributeTable.value] = it.value
+            this[PageAttributeTable.updatedAt] = now
+        },
+        body = {
+            it.applyInsert(this)
+        })
+}
 
 private data class PageAttributeStatement(
     val key: String,
