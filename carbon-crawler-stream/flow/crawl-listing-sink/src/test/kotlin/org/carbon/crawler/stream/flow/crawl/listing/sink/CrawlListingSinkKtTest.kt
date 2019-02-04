4b70f5be8bce76042cf478e2044aad7e139f0e51
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-04 23:22:29 +0900

    externalize compose function module and reference it

--- carbon-crawler-stream/flow/crawl-listing-sink/src/test/kotlin/org/carbon/crawler/stream/flow/crawl/listing/sink/CrawlListingSinkKtTest.kt
+++ carbon-crawler-stream/flow/crawl-listing-sink/src/test/kotlin/org/carbon/crawler/stream/flow/crawl/listing/sink/CrawlListingSinkKtTest.kt
@@ -1,27 +1,25 @@
 package org.carbon.crawler.stream.flow.crawl.listing.sink
 
-import org.carbon.composer.compose
-import org.carbon.crawler.model.extend.composer.RollbackTransaction
-import org.carbon.crawler.stream.core.config.DataConfig
+import org.carbon.crawler.model.extend.kompose.RollbackTransaction
+import org.carbon.crawler.stream.core.config.DataSourceConfig
 import org.carbon.crawler.stream.core.config.WebDriverConfig
 import org.carbon.crawler.stream.core.extend.selenium.DriverFactory
-import org.carbon.crawler.stream.message.crawlOrder.ListingOrderPayload
+import org.carbon.crawler.stream.message.TargetHostPayload
+import org.carbon.kompose.kompose
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("dev")
-@EnableConfigurationProperties
-@SpringBootTest(classes = [DataConfig::class, WebDriverConfig::class])
+@SpringBootTest(classes = [DataSourceConfig::class, WebDriverConfig::class])
 class CrawlListingSinkKtTest {
 
     @Autowired
     lateinit var driverFactory: DriverFactory
 
     @Test
-    fun test() = compose(RollbackTransaction) {
-        crawlListingSink(driverFactory)(ListingOrderPayload(1L))
+    fun test() = kompose(RollbackTransaction) {
+        crawlListingSink(driverFactory)(TargetHostPayload(1L))
     }
 }
