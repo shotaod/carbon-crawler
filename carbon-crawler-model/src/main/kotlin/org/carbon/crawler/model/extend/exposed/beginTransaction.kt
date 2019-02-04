439410d8379ff61edd22a685e5369202f157dffc
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-05 00:22:25 +0900

    modify sql logger and closure function

--- carbon-crawler-model/src/main/kotlin/org/carbon/crawler/model/extend/exposed/beginTransaction.kt
+++ carbon-crawler-model/src/main/kotlin/org/carbon/crawler/model/extend/exposed/beginTransaction.kt
@@ -1,6 +1,5 @@
 package org.carbon.crawler.model.extend.exposed
 
-import org.jetbrains.exposed.sql.SqlLogger
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.statements.BatchInsertStatement
 import org.jetbrains.exposed.sql.statements.StatementContext
@@ -10,34 +9,26 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import java.sql.PreparedStatement
 
-/**
- * @author Soda 2018/08/05.
- */
-private object Logger : SqlLogger {
-    private val logger = LoggerFactory.getLogger("org.carbon.sql.logger")
-    override fun log(context: StatementContext, transaction: Transaction) = logger.info(context.expandArgs(transaction))
-    fun log(message: String) = logger.info(message)
-}
-
 private object SqlLoggingInterceptor : StatementInterceptor {
+    private val logger = LoggerFactory.getLogger(SqlLoggingInterceptor::class.java)
     override fun afterExecution(transaction: Transaction, contexts: List<StatementContext>, executedStatement: PreparedStatement) {
-        val statement = contexts.first().statement
+        val batchStatement = contexts.first().statement
             as? BatchInsertStatement
-            ?: return contexts.forEach { Logger.log(it, transaction) }
+            ?: return contexts.forEach { logger.info(it.expandArgs(transaction)) }
 
-        val sql = statement.prepareSQL(transaction)
-        val sqlWithValues = statement.arguments().joinToString(separator = "\n  ", prefix = "$sql\n  ") { arg ->
+        val sql = batchStatement.prepareSQL(transaction)
+        val sqlWithValues = batchStatement.arguments().joinToString(separator = "\n  ", prefix = "$sql\n  ") { arg ->
             arg.joinToString(separator = ",", prefix = "(", postfix = ")") { (_, value) ->
                 value.toString()
             }
         }
-        Logger.log(sqlWithValues)
+        logger.info(sqlWithValues)
     }
 }
 
-fun <T> transactionL(statement: Transaction.() -> T): T {
+fun <T> beginTransaction(logging: Boolean = true, statement: Transaction.() -> T): T {
     val wrapStatement: Transaction.() -> T = {
-        registerInterceptor(SqlLoggingInterceptor)
+        if (logging) registerInterceptor(SqlLoggingInterceptor)
         statement()
     }
     return transaction(statement = wrapStatement)
