230ac5a36e41e2eebeca33589e7d91d84099f85f
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-04 23:09:17 +0900

    bug fix

--- carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/config/WebDriverConfig.kt
+++ carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/config/WebDriverConfig.kt
@@ -3,15 +3,15 @@ package org.carbon.crawler.stream.core.config
 import org.carbon.cloud.config.spring.WebDriverProp
 import org.carbon.crawler.stream.core.extend.selenium.DriverFactory
 import org.openqa.selenium.chrome.ChromeOptions
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import java.net.URL
 
 /**
  * @author Soda 2018/08/11.
  */
-@Import(WebDriverProp::class)
+@EnableConfigurationProperties(WebDriverProp::class)
 @Configuration
 class WebDriverConfig(
     val p: WebDriverProp
