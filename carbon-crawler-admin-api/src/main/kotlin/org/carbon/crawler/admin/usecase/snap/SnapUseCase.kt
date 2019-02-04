60836e6106e568115197690d2e081b80dd37f1d5
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-05 00:42:23 +0900

    split domain host -> (query | snapshot)

--- carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/usecase/snap/SnapUseCase.kt
+++ carbon-crawler-admin-api/src/main/kotlin/org/carbon/crawler/admin/usecase/snap/SnapUseCase.kt
@@ -4,7 +4,7 @@ import org.carbon.crawler.admin.www.Pager
 import org.carbon.crawler.admin.www.by
 import org.carbon.crawler.admin.www.v1.PagingUrlParameter
 import org.carbon.crawler.admin.www.v1.snap.SnapResponse.Companion.parseEntity
-import org.carbon.crawler.model.domain.QueryRepository
+import org.carbon.crawler.model.domain.SnapShotRepository
 import org.carbon.crawler.model.extend.kompose.Transaction
 import org.carbon.crawler.model.infra.record.HostTable
 import org.carbon.kompose.kompose
@@ -14,7 +14,7 @@ object SnapUseCase {
     fun fetchSnaps(param: PagingUrlParameter) = kompose(Transaction(logging = true)) {
         val (page, size) = param
 
-        val snaps = QueryRepository.fetch(page, size).map(::parseEntity)
+        val snaps = SnapShotRepository.fetch(page, size).map(::parseEntity)
         val count = HostTable.selectAll().count()
 
         snaps by Pager(page, count, size)
