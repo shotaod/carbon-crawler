42ec48bb80cf0f64267c0119a9b3f1318563538b
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-05 00:43:49 +0900

    externalize compose function module and reference it

--- carbon-crawler-admin-api/src/test/kotlin/org/carbon/crawler/admin/test/extend/kompose/TestModule.kt
+++ carbon-crawler-admin-api/src/test/kotlin/org/carbon/crawler/admin/test/extend/kompose/TestModule.kt
@@ -1,10 +1,10 @@
-package org.carbon.crawler.admin.extend.compose
+package org.carbon.crawler.admin.test.extend.kompose
 
 import io.ktor.application.Application
 import io.ktor.server.testing.withTestApplication
-import org.carbon.composer.Composable
+import org.carbon.kompose.Komposable
 
-class TestModule(private val module: Application.() -> Unit) : Composable<Unit>() {
+class TestModule(private val module: Application.() -> Unit) : Komposable<Unit>() {
     override fun invoke() {
         withTestApplication(module) {
             context.set(this)
