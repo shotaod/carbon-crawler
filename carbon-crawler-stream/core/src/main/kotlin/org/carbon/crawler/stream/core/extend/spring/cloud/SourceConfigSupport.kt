04377fd15d133cf093b6522dee52149e3367b32b
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-04 23:11:29 +0900

    use PollableSource

--- carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/extend/spring/cloud/SourceConfigSupport.kt
+++ carbon-crawler-stream/core/src/main/kotlin/org/carbon/crawler/stream/core/extend/spring/cloud/SourceConfigSupport.kt
@@ -1,64 +1,16 @@
 package org.carbon.crawler.stream.core.extend.spring.cloud
 
-import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.messaging.Source
-import org.springframework.context.annotation.Bean
-import org.springframework.integration.core.MessageSource
-import org.springframework.integration.dsl.IntegrationFlow
-import org.springframework.integration.dsl.IntegrationFlows
-import org.springframework.integration.endpoint.AbstractMessageSource
-import org.springframework.integration.scheduling.PollerMetadata
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.function.Consumer
+import org.springframework.integration.annotation.InboundChannelAdapter
+import org.springframework.integration.annotation.Poller
 
-typealias TypedSource<PAYLOAD> = () -> PAYLOAD?
-
-/**
- * Configuration Support for Spring Cloud [MessageSource]
- * with configuring default poller
- * ``````
- * sample usage
- * ``````
- * ```kotlin
- * @EnableBinding(Source::class)
- * @Configuration
- * class SourceStreamConfig(poller: PollerMetadata)
- *   : SourceConfigSupport<CrawlOrder>(poller) {
- *   override val messageSource: TypedSource<Some> = { Some() }
- * }
- * ```
- *
- * @param OUT type of message body
- *
- * @author Soda 2018/08/08.
- */
-abstract class SourceConfigSupport<OUT>(
-    private var poller: PollerMetadata,
-    private val channel: Source,
-    private val source: TypedSource<OUT>
-) {
-    companion object {
-        val count = AtomicInteger(0)
-    }
-
-    val logger = LoggerFactory.getLogger(this.javaClass)!!
-
-    fun <T> TypedSource<T>.toSource(): MessageSource<T> = object : AbstractMessageSource<T>() {
-        override fun getComponentType(): String = "source:${this::class.simpleName}#${count.incrementAndGet()}"
-        override fun doReceive(): T? {
-            val result: T? = this@toSource.invoke()
-            when (result) {
-                null -> logger.info("[source] null")
-                else -> logger.info("[source] $result")
-            }
-
-            return result
-        }
-    }
-
-    @Bean
-    open fun sourceFlow(): IntegrationFlow = IntegrationFlows
-        .from(source.toSource(), Consumer { it.poller(poller) })
-        .channel(channel.output())
-        .get()
-}
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@InboundChannelAdapter(
+    value = Source.OUTPUT,
+    poller = [Poller(
+        maxMessagesPerPoll = "\${trigger.maxMessage:}",
+        fixedDelay = "\${trigger.fixedDelay:}",
+        cron = "\${trigger.cron:}"
+    )])
+annotation class PollableSource
