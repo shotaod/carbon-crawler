bee5f8eb161c76090ed6d667de3f4bed20d0cb45
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-05 00:23:03 +0900

    externalize compose function module and reference it

--- carbon-crawler-model/src/main/kotlin/org/carbon/crawler/model/extend/kompose/Komposer.kt
+++ carbon-crawler-model/src/main/kotlin/org/carbon/crawler/model/extend/kompose/Komposer.kt
@@ -1,26 +1,24 @@
-package org.carbon.crawler.model.extend.composer
+package org.carbon.crawler.model.extend.kompose
 
-import org.carbon.composer.Composable
-import org.carbon.crawler.model.extend.exposed.transactionL
+import org.carbon.crawler.model.extend.exposed.beginTransaction
 import org.carbon.crawler.model.infra.meta.Meta
+import org.carbon.kompose.Komposable
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.deleteAll
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 
-class Transaction<T>(private val logging: Boolean = false) : Composable<T>() {
-    override fun invoke(): T =
-        if (logging) transactionL { super.callChild() }
-        else transaction { super.callChild() }
+class Transaction<T>(private val logging: Boolean = false) : Komposable<T>() {
+    override fun invoke(): T = beginTransaction(logging) { super.callChild() }
 }
 
 interface DBUtil {
     fun clean()
 }
 
-object RollbackTransaction : Composable<Unit>() {
+object RollbackTransaction : Komposable<Unit>() {
     private object RollbackException : Exception("for Rollback purpose")
-    private class DBUtilImpl : DBUtil {
+    object DBUtilImpl : DBUtil {
         override fun clean() {
             SchemaUtils.create(*Meta.tablesByAncestor)
             Meta.tablesByAncestor
@@ -31,9 +29,10 @@ object RollbackTransaction : Composable<Unit>() {
 
     private val log = LoggerFactory.getLogger(RollbackTransaction::class.java)
     override fun invoke() {
-        context.setAs(DBUtilImpl(), DBUtil::class)
+        context.setAs(DBUtilImpl, DBUtil::class)
         try {
-            transactionL {
+            beginTransaction {
+                context.setAs(this, Transaction::class)
                 super.callChild()
                 throw RollbackException
             }
