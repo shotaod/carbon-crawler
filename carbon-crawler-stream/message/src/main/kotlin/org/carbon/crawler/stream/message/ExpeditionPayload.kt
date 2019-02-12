a1ef5e8cb27ee3b168bae3e1fa48cdfa5b2ddfe6
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-02-12 21:25:54 +0900

    update message content

--- carbon-crawler-stream/message/src/main/kotlin/org/carbon/crawler/stream/message/ExpeditionPayload.kt
+++ carbon-crawler-stream/message/src/main/kotlin/org/carbon/crawler/stream/message/ExpeditionPayload.kt
@@ -3,6 +3,6 @@ package org.carbon.crawler.stream.message
 /**
  * @author Soda 2018/08/09.
  */
-data class TargetHostPayload(
-    val hostId: Long
+data class ExpeditionPayload(
+    val id: String
 )
